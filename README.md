e3dc6a0fa35d269f053b24309d8b119587f27d13
thealisalehi96dorsa <108278103+thealisalehi96dorsa@users.noreply.github.com>
2023-08-05 08:54:46 +0330

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# dorsa_help
-This repo contains dorsa help module to create help page for apps
+# Help_Viewer
+
+Help-Viewer is a PyQt-based module developed to create a help-viewer object. The module uses a PDF explorer API to load PDF help files.
+
+## Features
+    - PDF explorer to load pdf files
+    - Exploring, zooming in/out, highlighting, editing, saving and printing pdf file
+
+## Notice
+This module is completely PyQt5-based and it's not possible to be used with other non-Qt-based applications at this moment.
+
+## Dependencies
+    - PyQt5
+    - filetype
+
+## Installation
+    1- Install dependencies using pip install
+    2- clone Help-Viewer repository
+
+## Documentation
+Sphinx documents are not available at this moment.
+
+## Quick Start
+To use the Help-Viewer module, follow these steps:
+
+1. Import the module: 
+``` python
+import help_viewer
+```
+
+2. Create a `Help-Viewer` object inside a PyQt QMainWindow object and assign it to a frame object:
+``` python
+qmainwindow_obj.help_viewer = help_viewer.HelpViewer(pdfjs_root_path=os.path.dirname(sys.argv[0]))
+qmainwindow_obj.helpviewer_frame.layout().addWidget(qmainwindow_obj.help_viewer)
+```
+#### Input parameters:
+    - pdfjs_root_path: It is the path to pdfjs folder included with the repo. The pdfjs API is used to build PDF-explorer module
+
+3. Add a PDF file
+``` python
+res, message = qmainwindow_obj.help_viewer.load_pdf_file(pdf_address=pdf_path)
+```
+#### Input parameters:
+    - pdf_address: Path of the pdf file
+    
+# Main functions
+
+## Load a PDF file
+``` python
+res, message = qmainwindow_obj.help_viewer.load_pdf_file(pdf_address=pdf_path)
+```
+#### Input parameters:
+    - pdf_address: Path of the pdf file
+
+## Remove/clear Help-Viewer pdf file
+```python
+qmainwindow_obj.help_viewer.close_file()
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ help_viewer.py
@@ -0,0 +1,105 @@
+"""
+########################################################
+This PyQt-based module is developed to create a help-viewer object.
+The module uses a PDF explorer API to load PDF help files
+
+Dependencies:
+    This module is based on PyQt5 QtWebEngineWidgets. So, to use this module in your code you must have PyQt5 library installed.
+    - PyQt5
+    - filetype
+
+Features:
+    - PDF explorer to load pdf files
+    - Exploring, zooming in/out, highlighting, editing, saving and printing pdf file
+
+Notice:
+    This module is completely PyQt5-based and it's not possible to be used with other non-Qt-based applications at this moment.
+
+Designed and developed by Ali Salehi
+########################################################
+"""
+
+
+from PyQt5 import QtCore, QtWebEngineWidgets
+import filetype
+
+
+
+class HelpViewer(QtWebEngineWidgets.QWebEngineView):
+
+    def __init__(self, pdfjs_root_path: str):
+        """This class is used to build the Help-Viewer object to show help files
+
+        :param pdfjs_root_path: root path of pdfjs folder
+        :type pdfjs_root_path: str
+        """
+
+        super(HelpViewer, self).__init__()
+
+        # PDF explorer API path
+        self.pdfjs_path = 'file:///%s/pdfjs/web/viewer.html' % (pdfjs_root_path)
+
+        # create PDF explorer module
+        self.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
+        self.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PdfViewerEnabled, True)
+        self.showMaximized()
+    
+
+    def load_pdf_file(self, pdf_address: str):
+        """This function is used to load a pdf file from path and set to PDF-Viewer
+
+        :param pdf_address: pdf file path
+        :type pdf_address: str
+        :return: Tuple of Boolean detemining pdf loading is successfull and message
+        :rtype: (boolean, str)
+        """
+
+        try:
+            if not self.is_pdf(pdf_address=pdf_address):
+                return False, ''
+            
+            url = '%s?file=%s' % (self.pdfjs_path, pdf_address)
+            pdf_url = QtCore.QUrl(QtCore.QUrl.fromUserInput(self.convert_pdf_address(pdf_address=url)))
+            pdf_url.setFragment("page=1")
+            self.load(pdf_url)
+            self.showMaximized()
+
+            return True, ''
+        
+        except Exception as e:
+            self.close_file()
+            return False, e
+    
+
+    def close_file(self):
+        """This function is used to close current help file
+        """
+
+        try:
+            self.close()
+            return
+        
+        except:
+            return
+    
+    
+    def convert_pdf_address(self, pdf_address: str):
+        """This function is used to convert pdf address to form that can be loaded by PDF-Explorer module
+
+        :param address: pdf file path
+        :type address: str
+        :return: _description_
+        :rtype: _type_
+        """
+
+        return pdf_address.replace('\\', '/')
+
+
+    def is_pdf(self, pdf_address: str):
+        """This function is used to check if input file is a valid pdf
+
+        :param pdf_address: pdf file path
+        :type pdf_address: str
+        """
+
+        return filetype.guess(pdf_address).mime == 'application/pdf'
